964372644869d558ff97315e68f870ec5d6a0cb3
trungld3 <k21tpm4@gmail.com>
2020-05-19 19:52:02 +0700

    Initial Commit

---
+++ What's The Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What's The Weather
+//
+//  Created by TrungLD on 5/19/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
